cb33e4d6f143f05513fcbd6e8e646f1d36b11fda
dzaky970 <roswitaagestina@gmail.com>
2025-04-13 12:29:08 +0800

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Contoh dengan External JS</title>
+</head>
+<body>
+  <h1>Cek Console dan Alert</h1>
+  <script src="script.js"></script>
+</body>
+</html>
